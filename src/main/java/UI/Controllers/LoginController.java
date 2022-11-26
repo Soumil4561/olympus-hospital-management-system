8b7e4fb656f9431aee79f949387696b91a580f0e
Manish Gudewar <202152322@iiitvadodara.ac.in>
2022-11-26 21:49:26 +0530

    trying action event

--- src/main/java/UI/Controllers/LoginController.java
+++ src/main/java/UI/Controllers/LoginController.java
@@ -2,6 +2,7 @@ package UI.Controllers;
 
 import UI.Elements.PopUpBox;
 import UI.Functions.JumpScene;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -26,7 +27,7 @@ public class LoginController {
     private TextField uid_input;
 
     @FXML
-    void gotoStaffScene(MouseEvent event) throws IOException, SQLException {
+    void gotoStaffScene(ActionEvent event) throws IOException, SQLException {
         uid = Long.parseLong(uid_input.getText());
         pass = pass_input.getText();
         String typeOfStaff=LoginRequest(uid,pass);
@@ -45,6 +46,28 @@ public class LoginController {
             }
         }
     }
+
+
+//    @FXML
+//    void gotoStaffHome(ActionEvent event) throws SQLException {
+//        uid = Long.parseLong(uid_input.getText());
+//        pass = pass_input.getText();
+//        String typeOfStaff=LoginRequest(uid,pass);
+//        if(typeOfStaff.equals("Receptionist")){
+//            JumpScene.changeScene(loginPane,"UI/homeTab.fxml",event);
+//        }
+//        else if(typeOfStaff.equals("Doctor")){
+//            JumpScene.changeScene(loginPane,"UI/homeTabDoctor.fxml",event);
+//
+//        }
+//        else {
+//            try {
+//                PopUpBox.display("Wrong Username or Password");
+//            } catch (Exception e){
+//                System.out.println("cant open alertbox");
+//            }
+//        }
+//    }
 
     public String LoginRequest(long id,String password) throws SQLException {
        return Staff.Login(id,password);
